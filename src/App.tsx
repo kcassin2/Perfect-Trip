e22bcffeb7a1009303df026a9a77c68a74c80042
kcassin2 <31949074+kcassin2@users.noreply.github.com>
2020-04-29 13:51:28 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import "@codetrix-studio/capacitor-google-auth";
+import { Plugins } from '@capacitor/core';
+import Weather from "./components/weather";
+
 import {
   IonApp,
   IonIcon,
@@ -10,10 +14,12 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle,images,  airplaneOutline, mapOutline, personOutline, searchCircleOutline, basketOutline } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -30,37 +36,50 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
+//import openWeatherMapAPI from "./keys";
 /* Theme variables */
 import './theme/variables.css';
+
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
+
       <IonTabs>
         <IonRouterOutlet>
           <Route path="/tab1" component={Tab1} exact={true} />
           <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
+          <Route path="/tab3" component={Tab3} exact={true}/>
+          <Route path="/tab4" component={Tab4}/>
+          
           <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+         
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon icon={airplaneOutline} />
+            <IonLabel>Popular Trips</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon icon={mapOutline} />
+            <IonLabel>Saved Trips</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon icon={searchCircleOutline} />
+            <IonLabel>Search</IonLabel>
           </IonTabButton>
+
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon icon={basketOutline} />
+            <IonLabel>Cart</IonLabel>
+          </IonTabButton>
+
         </IonTabBar>
       </IonTabs>
     </IonReactRouter>
   </IonApp>
+  
 );
 
 export default App;
+
